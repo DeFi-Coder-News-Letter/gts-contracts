237386c1e30c07352a64fa4506ef1d7fed8016e3
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-04-17 18:16:27 -1000

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
-const PAX = artifacts.require('PAXImplementation');
+const GTS = artifacts.require('GTSImplementation');
 const Proxy = artifacts.require('AdminUpgradeabilityProxy');
 
 module.exports = async function(deployer) {
   await deployer;
 
-  await deployer.deploy(PAX);
-  const proxy = await deployer.deploy(Proxy, PAX.address);
-  const proxiedPAX = await PAX.at(proxy.address);
+  await deployer.deploy(GTS);
+  const proxy = await deployer.deploy(Proxy, GTS.address);
+  const proxiedGTS = await GTS.at(proxy.address);
   await proxy.changeAdmin("0xf0b1eef88956b0a307fa87b5f5671aad6a5d330f");
-  await proxiedPAX.initialize();
+  await proxiedGTS.initialize();
 };
