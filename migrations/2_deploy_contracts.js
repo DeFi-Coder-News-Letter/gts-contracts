3a38547f6d2f19cb817c1cbe33599245a653a574
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-04-18 09:13:00 -1000

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,6 +7,7 @@ module.exports = async function(deployer) {
   await deployer.deploy(GTS);
   const proxy = await deployer.deploy(Proxy, GTS.address);
   const proxiedGTS = await GTS.at(proxy.address);
-  await proxy.changeAdmin("0xf0b1eef88956b0a307fa87b5f5671aad6a5d330f");
+ // await proxy.changeAdmin("0xf0b1eef88956b0a307fa87b5f5671aad6a5d330f");
+  await proxy.changeAdmin("0x<adminAddressWallet");
   await proxiedGTS.initialize();
 };
