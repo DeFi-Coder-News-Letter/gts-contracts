8ffbe82dc002491648c741ab3eae88fac06f13b4
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-04-17 18:19:52 -1000

    Update CanReclaimFundsTest.js

--- test/CanReclaimFundsTest.js
+++ test/CanReclaimFundsTest.js
@@ -1,21 +1,21 @@
-const PAXMock = artifacts.require('PAXWithBalance.sol');
+const GTSMock = artifacts.require('GTSWithBalance.sol');
 const Proxy = artifacts.require('AdminUpgradeabilityProxy.sol');
 
 const assertRevert = require('./helpers/assertRevert');
 
-// Test that the PAX contract can reclaim PAX it has received.
+// Test that the GTS contract can reclaim GTS it has received.
 // Note that the contract is not payable in Eth.
 contract('CanReclaimFunds', function ([_, admin, owner, assetProtectionRole, anyone]) {
   const amount = 100;
 
   beforeEach(async function () {
     // Create contract and token
-    const pax = await PAXMock.new({from: owner});
-    const proxy = await Proxy.new(pax.address, {from: admin});
-    const proxiedPAX = await PAXMock.at(proxy.address);
-    await proxiedPAX.initialize({from: owner});
-    await proxiedPAX.initializeBalance(owner, amount);
-    this.token = proxiedPAX;
+    const gts = await GTSMock.new({from: owner});
+    const proxy = await Proxy.new(gts.address, {from: admin});
+    const proxiedGTS = await GTSMock.at(proxy.address);
+    await proxiedGTS.initialize({from: owner});
+    await proxiedGTS.initializeBalance(owner, amount);
+    this.token = proxiedGTS;
 
     // Send token to the contract
     await this.token.transfer(this.token.address, amount, {from: owner});
@@ -36,7 +36,7 @@ contract('CanReclaimFunds', function ([_, admin, owner, assetProtectionRole, any
   });
 
   it('should allow owner to reclaim tokens', async function () {
-    await this.token.reclaimPAX({from: owner});
+    await this.token.reclaimGTS({from: owner});
 
     const balance = await this.token.balanceOf(owner);
     assert.equal(amount, balance);
@@ -46,7 +46,7 @@ contract('CanReclaimFunds', function ([_, admin, owner, assetProtectionRole, any
 
   it('should allow only owner to reclaim tokens', async function () {
     await assertRevert(
-      this.token.reclaimPAX({from: anyone})
+      this.token.reclaimGTS({from: anyone})
     );
   });
 });
